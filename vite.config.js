51d8cccad3d590ee649eb660e6817526dc54d0d8
Angelina <pretzel318@gmail.com>
2023-02-05 22:41:57 +0400

    added aliases

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url))
     }
   }
 })
