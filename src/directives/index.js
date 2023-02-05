ed60fad54a047de46c27a3ed320d5c4187796382
Angelina <pretzel318@gmail.com>
2023-02-05 22:42:21 +0400

    added directives

---
+++ src/directives/index.js
@@ -0,0 +1,4 @@
+import VFocus from "./VFocus";
+import VIntersecton from "./VIntersecton";
+
+export default [VIntersecton, VFocus];
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import components from "./components/UI";
+import components from "@components/UI";
 import router from "./router/router";
+import directives from "./directives";
 
 import "./index.css";
 
@@ -10,5 +11,10 @@ const app = createApp(App);
 components.forEach(cmpnt => {
   app.component(cmpnt.name, cmpnt);
 });
+
+directives.forEach(directive => {
+  app.directive(directive.name, directive);
+});
+
 
 app.use(router).mount("#app");
